3f056a0625cb8e41bdfd18f90b8ed7d82818c272
Michael <michaelmccabe711@gmail.com>
2019-09-15 14:41:41 -0400

    Adding more constraints on to createValidSectionBacktracking function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 
 const { randomWordGenerator, randomNumberGenerator, printGrid } = require("./helperMethods");
 const { insertFirstHorizontalEntry, insertFirstHorizontalWord } = require("./firstEntrySectionMethods");
-const { insertHorizontalWordFromPartial } = require("./laterEntryMethods")
+const { insertHorizontalWordFromPartial, insertVerticalEntry } = require("./laterEntryMethods")
 const { isGridValidHorizontally, 
     isGridValidVertically, 
     isGridPartialWordValidHorizontally, 
@@ -13,7 +13,9 @@ const { dictionary} = require("./dictionary/dictionary")
 const { blankGrid } = require("./getPreDeterminedGrid");
 
 let grid
-let NYTcrossword = createNYTcrossword(grid,blankGrid)
+let gridHistory = []
+gridHistory.push(blankGrid)
+let NYTcrossword = createNYTcrossword(grid,blankGrid, gridHistory)
 printGrid(NYTcrossword)
 
 function createNYTcrossword(grid, blankGrid) {
@@ -61,19 +63,19 @@ function createNYTcrossword(grid, blankGrid) {
         }
         // else: continue the loop with a blank grid
     }
-
     // If execution is here, we have a valid horizontal word on the bottom left of our crossword
     // We also have vertical words "growing" from the horizontal word we put down
     // The crossword is valid horizontally and vertically
-    // The crossword is ALSO has partial words that are valid horizontally - 
+    // The crossword is ALSO has partial words that are valid horizontally
+    gridHistory.push(grid)
 
     // the below while loop is responsible for filling out the rest of the board
     let isCrosswordComplete = false;
     while( isCrosswordComplete === false ) {
         // Note: Handle case of "islands" of white cells in the future
-        grid = createValidSectionBacktracking(grid);
+        grid = createValidSectionBacktracking(grid, gridHistory);
         isCrosswordComplete = isGridComplete(grid);
-        break; // break for testing, won't be here for production
+        // break; // break for testing, won't be here for production
     }
 
     // If execution is here, the grid is fully complete
@@ -85,8 +87,14 @@ function createNYTcrossword(grid, blankGrid) {
     return grid;
 }
 
-function createValidSectionBacktracking(grid) {
-    // The first time we are in here, "grid" has the properties:
+function createValidSectionBacktracking(grid, gridHistory) {
+    let count = 0
+    // At this point, we have a perfectly valid section of words in the bottom-left corner of the crossword
+    // we have a horizontal word at the bottom left, with words "growing" vertically from that word
+    // All words are valid horizontally and vertically
+    // All partial words going horizontally are able to form words (vertial has no partials by nature of the algorithm so far)
+
+    // All incoming "grids" have the properties:
     // horizontalWordPartial & verticalWordPartial
     // - minWordsArr (type of array)
     // - minWordsColLocation
@@ -99,161 +107,96 @@ function createValidSectionBacktracking(grid) {
     // verticalWords
     // - verticalWords
     // - validity
-    let gridComplete = false
-    while (gridComplete === false) {
+
+    // gridComplete should be when there are no partials left (we have completed an entire connected white-cell section)
+    let gridConnectedSectionComplete = false
+    while (gridConnectedSectionComplete === false) {
+        count++
+        let minValidPartialWords
+        if(gridHistory.length == 2) {
+            // In the beginning, a crossword board is not valid if we can only make 4 words or less from a partial word
+            minValidPartialWords = 4
+        } else {
+            // later, the validity will be 1 or less
+            minValidPartialWords = 1
+        }
+
+        // if some computation is taking too long, go back one result and do that section again
+        if(gridHistory.length === 3 && count === 4000) {
+            console.log('go back one result')
+            gridHistory.pop()
+            grid = gridHistory[gridHistory.length - 1]
+            printGrid(grid)
+            count = 0
+        }
+        
+
         // we create a deep clone of the incoming grid, which allows us to backtrack if our newGrid is invalid
         let dummyGrid = _.cloneDeep(grid);
+        dummyGrid.horizontalWordPartial = grid.horizontalWordPartial
+        dummyGrid.horizontalWords = grid.horizontalWords
+        dummyGrid.verticalWordPartial = grid.verticalWordPartial
+        dummyGrid.verticalWords = grid.verticalWords
 
-        console.log(dummyGrid)
+        let minVerticalPartial = grid.verticalWordPartial
+        let minHorizontalPartial = grid.horizontalWordPartial
 
-        // insert a horizontal word on a "horizontal partial word" entry that has the least amount of possible words
-        // FUTURE: I will need to test to make sure that the inserted word IS VALID WITH PARTIAL WORDS
-        // Step #1: RNG a valid word from the list of "least amount of possible words" horizontally- minWordsArr property
-        let leastNumberWordsList = grid.horizontalWordPartial.minWordsArr
-        let rngHorizontalWordIdx = randomNumberGenerator(leastNumberWordsList.length)
-        let horizontalWordForPartial = leastNumberWordsList[rngHorizontalWordIdx]
-
-        dummyGrid = insertHorizontalWordFromPartial(grid, horizontalWordForPartial)
-        // console.log(dummyGrid)
-        // printGrid(dummyGrid)
-
-        // Next we will insert vertical words as follows:
-        // // begin to insert vertical words
-        // // we will insert words from entry.startLeft to entry.endRight for entry.row
-        // for (let i = entry.startLeft; i <= entry.endRight; i++) {
-        //     newGrid = insertVerticalEntry(newGrid, entry.row, i);
-        // }
-        // newGrid.horizontalWords; // not really needed
-
-        // // if the grid is valid, return the new grid
-        // // if the grid is not valid, return the old grid we put in, and do another "dice roll"
-        // // printGrid(newGrid)
-        // let horizontalAnalysis = isGridValidHorizontally(newGrid);
-        // let horizontalWordPartialAnalysis = isGridPartialWordValidHorizontally(newGrid, 4)
-        // let verticalAnalysis = isGridValidVertically(newGrid)
-
-        // if (horizontalAnalysis.validity === true && verticalAnalysis.validity === true && horizontalWordPartialAnalysis.validity === true) {
-        //     newGrid.horizontalWords = horizontalAnalysis;
-        //     newGrid.verticalWords = verticalAnalysis
-        //     newGrid.horizontalWordPartial = horizontalWordPartialAnalysis
-        //     return newGrid;
-        // }
-        // return grid;
-
-        isGridComplete(grid)
-        break // temorary break
-    }
-    return grid;
-}
-
-
-/* 
-The following function takes some cell location in the grid
-From that cell location, it travels as far up and as far down as possible to find:
-1. Where to start the vertical word
-2. How big the vertical word must be
-
-Extra Notes:
-This function also has the ability to NOT OVERWRITE OTHER WORDS
-*/
-function insertVerticalEntry(grid, row, col, dictionary) {
-    // where do we want to begin the word coming down?
-
-    // iterate up the column until we hit either a null or undefined for a start
-    let rowPointer = row;
-    let rowPointerValue = grid[rowPointer][col];
-    while (rowPointerValue !== undefined && rowPointerValue !== null) {
-        rowPointer--;
-        if (grid[rowPointer] === undefined) {
-            break;
-        }
-        rowPointerValue = grid[rowPointer][col];
-    }
-    let rowStart = rowPointer + 1;
-
-    // iterate down the column until we hit either a null or undefined for an end
-    // while we do this, if we find a letter, we will store that letter and its location in the hash
-    // this hash indicates the particular existing characters and their locations
-    // we will use this hash to not overwrite these existing characters
-    let charsInclude = {};
-    let hashPointer = 0;
-
-    rowPointer = rowStart;
-    rowPointerValue = grid[rowPointer][col];
-    while (rowPointerValue !== undefined && rowPointerValue !== null) {
-        charsInclude[hashPointer] = rowPointerValue;
-        hashPointer++;
-        rowPointer++;
-        if (grid[rowPointer] === undefined) {
-            break;
-        }
-        rowPointerValue = grid[rowPointer][col];
-    }
-
-    let rowEnd = rowPointer - 1;
-
-    // we now have a hash of characters and their locations that must belong in the incoming word
-    // we can also figure out the wordHeight from our pointers
-
-    // insert a valid word from rowStart to rowEnd down vertically
-    // Valid includes: 1) proper height, 2) does not destroy other words (adheres to charsInclude hash)
-    let wordHeight = rowEnd - rowStart + 1;
-
-    // Find a random word that satisisfies the criteria of letters and their locations, found inside the hash
-    let randomWord = null;
-    while (randomWord === null) {
-        // first, find a word of the same length as wordHeight
-        while (randomWord === null) {
-            let testvariable = 1 // NOTE: execution seems to get stuck on the above while loop 1/20-ish times
-            randomWord = randomWordGenerator(dictionary);
-            if (randomWord.length == wordHeight) {
-                break;
-            } else {
-                randomWord = null;
-            }
-        }
-
-        // We now have a word that is the same length as wordHeight
-
-        // next, see if that word adheres to our hash
-        // we go through every letter of the random word, and check it against our hash
-        // "1" means "free space"
-
-        let randomWordPointer = 0;
-        while (randomWordPointer < wordHeight) {
-            if (charsInclude[randomWordPointer] === 1) {
-                // this is the free space, you may continue
-            } else if (
-                charsInclude[randomWordPointer] !== randomWord[randomWordPointer]
-            ) {
-                // execution is here when we end up on an a space with an existing character (defined by the hash) and that letter-location does NOT equal the letter-location of the random word
-                // if we hit this, we must break out of for this loop and find another random word
-                break;
-            }
-            // continue the loop if:
-            // 1: the hash says that, at a particular location, we have a 1 - which is a "free space"
-            // 2: the hash has a character at a particular location, and our RNG word has a character at that location as well
-            randomWordPointer++;
-        }
-
-        // if we passed all of the tests, break out of the while loop
-        if (randomWordPointer === wordHeight) {
+        // The first word we lay down targets the partial word that can form the least amount of words
+        // If the smallest number of words that we can lay down comes from a partial word going vertically, focus on filling in that horizontal partial word
+        if(minVerticalPartial.numMinWordCombinations < minHorizontalPartial.numMinWordCombinations ) {
+            // put code here
             break;
         } else {
-            randomWord = null;
-        }
+            // Else: if the smallest number of words that we can lay down comes from a partial word going horizontally, focus on filling in that horizontal partial word
+            // insert a horizontal word on a "horizontal partial word" entry that has the least amount of possible words
+            // FUTURE: I will need to test to make sure that the inserted word IS VALID WITH PARTIAL WORDS
+            // Step #1: RNG a valid word from the list of "least amount of possible words" horizontally- minWordsArr property
+            let leastNumberWordsList = minHorizontalPartial.minWordsArr
+            let rngHorizontalWordIdx = randomNumberGenerator(leastNumberWordsList.length)
+            let horizontalWordForPartial = leastNumberWordsList[rngHorizontalWordIdx]
+            dummyGrid = insertHorizontalWordFromPartial(dummyGrid, horizontalWordForPartial)
+            // console.log(horizontalWordForPartial)
+            // printGrid(dummyGrid)
+
+            // Next we will insert vertical words as follows:
+            // we will insert words from entry.startLeft to entry.endRight for entry.row
+            let partialWordBegin = minHorizontalPartial.minWordsColLocation
+            let partialWordEnd = partialWordBegin + minHorizontalPartial.minWordsArr[0].length
+            let partialWordRow = minHorizontalPartial.minWordsRowLocation
+            for (let i = partialWordBegin; i < partialWordEnd; i++) {
+                dummyGrid = insertVerticalEntry(dummyGrid, partialWordRow, i, dictionary);
+            }
+            dummyGrid.horizontalWords; // not really needed
+            // printGrid(dummyGrid)
+
+            // if the grid is valid, return the new grid
+            // if the grid is not valid, return the old grid we put in, and do another "dice roll"
+            // printGrid(dummyGrid)
+            let horizontalAnalysis = isGridValidHorizontally(dummyGrid);
+            let horizontalWordPartialAnalysis = isGridPartialWordValidHorizontally(dummyGrid, minValidPartialWords)
+            let verticalAnalysis = isGridValidVertically(dummyGrid)
+            // below should always be true if this is our first section, we are using it for safety
+            let verticalWordPartialAnalysis = isGridPartialWordValidVertically(dummyGrid, minValidPartialWords)
+        
+            if (horizontalAnalysis.validity === true && verticalAnalysis.validity === true && horizontalWordPartialAnalysis.validity === true && verticalWordPartialAnalysis.validity === true) {
+                dummyGrid.horizontalWords = horizontalAnalysis;
+                dummyGrid.verticalWords = verticalAnalysis
+                dummyGrid.horizontalWordPartial = horizontalWordPartialAnalysis
+                dummyGrid.verticalWordPartial = verticalWordPartialAnalysis
+
+                // Now that this "dummyGrid" is proved to be valid, we will set is as "grid" - because it is valid
+                grid = dummyGrid
+                gridHistory.push(grid)
+                count = 0
+                console.log('**hit**')
+                printGrid(grid)
+                // have we completed the entire connected white-cell section?
+                // If a horizontal or partial word analysis yields MAX_VALUE as the number of "minimum word combinations", there are no partial words to be found
+                gridConnectedSectionComplete = grid.verticalWordPartial.numMinWordCombinations === Number.MAX_VALUE && grid.horizontalWordPartial.numMinWordCombinations === Number.MAX_VALUE ? true : false
+            }
+            // Else: The dummyGrid is not valid, therefore we will now revert back to the previous "grid"
+        }     
     }
-
-    // if execution is here, we have a valid vertical word that adheres to our constraints found in our hash
-    // We will now insert that word in the desired location
-
-    let insertPointer = 0;
-    for (let i = rowStart; i <= rowEnd; i++) {
-        grid[i][col] = randomWord[insertPointer];
-        insertPointer++;
-    }
-
-    return grid;
 }
 
 /*
--- laterEntryMethods.js
+++ laterEntryMethods.js
@@ -1,3 +1,6 @@
+const { randomWordGenerator} = require("./helperMethods");
+const _ = require("lodash");
+
 exports.insertHorizontalWordFromPartial = function(grid, horizontalWord) {
 
     let wordLocation = grid.horizontalWordPartial
@@ -9,4 +12,119 @@ exports.insertHorizontalWordFromPartial = function(grid, horizontalWord) {
     }
 
     return grid
+}
+
+/* 
+The following function takes some cell location in the grid
+From that cell location, it travels as far up and as far down as possible to find:
+1. Where to start the vertical word
+2. How big the vertical word must be
+
+Extra Notes:
+This function also has the ability to NOT OVERWRITE OTHER WORDS
+*/
+exports.insertVerticalEntry = function(grid, row, col, dictionary) {
+    // where do we want to begin the word coming down?
+
+    // iterate up the column until we hit either a null or undefined for a start
+    let rowPointer = row;
+    let rowPointerValue = grid[rowPointer][col];
+    while (rowPointerValue !== undefined && rowPointerValue !== null) {
+        rowPointer--;
+        if (grid[rowPointer] === undefined) {
+            break;
+        }
+        rowPointerValue = grid[rowPointer][col];
+    }
+    let rowStart = rowPointer + 1;
+
+    // iterate down the column until we hit either a null or undefined for an end
+    // while we do this, if we find a letter, we will store that letter and its location in the hash
+    // this hash indicates the particular existing characters and their locations
+    // we will use this hash to not overwrite these existing characters
+    let charsInclude = {};
+    let hashPointer = 0;
+
+    rowPointer = rowStart;
+    rowPointerValue = grid[rowPointer][col];
+    while (rowPointerValue !== undefined && rowPointerValue !== null) {
+        charsInclude[hashPointer] = rowPointerValue;
+        hashPointer++;
+        rowPointer++;
+        if (grid[rowPointer] === undefined) {
+            break;
+        }
+        rowPointerValue = grid[rowPointer][col];
+    }
+
+    let rowEnd = rowPointer - 1;
+
+    // we now have a hash of characters and their locations that must belong in the incoming word
+    // we can also figure out the wordHeight from our pointers
+
+    // if we do not have any 1's in our charsInclude hash, that means that this particular word is NOT a partial, it is filled out
+    // If this is the case, stop the function and return the un-touched grid
+    if(_.includes(charsInclude, 1) !== true) {
+        return grid
+    }
+
+    // insert a valid word from rowStart to rowEnd down vertically
+    // Valid includes: 1) proper height, 2) does not destroy other words (adheres to charsInclude hash)
+    let wordHeight = rowEnd - rowStart + 1;
+
+    // Find a random word that satisisfies the criteria of letters and their locations, found inside the hash
+    let randomWord = null;
+    while (randomWord === null) {
+        // first, find a word of the same length as wordHeight
+        while (randomWord === null) {
+            let testvariable = 1 // NOTE: execution seems to get stuck on the above while loop 1/20-ish times
+            randomWord = randomWordGenerator(dictionary);
+            if (randomWord.length == wordHeight) {
+                break;
+            } else {
+                randomWord = null;
+            }
+        }
+
+        // We now have a word that is the same length as wordHeight
+
+        // next, see if that word adheres to our hash
+        // we go through every letter of the random word, and check it against our hash
+        // "1" means "free space"
+
+        let randomWordPointer = 0;
+        while (randomWordPointer < wordHeight) {
+            if (charsInclude[randomWordPointer] === 1) {
+                // this is the free space, you may continue
+            } else if (
+                charsInclude[randomWordPointer] !== randomWord[randomWordPointer]
+            ) {
+                // execution is here when we end up on an a space with an existing character (defined by the hash) and that letter-location does NOT equal the letter-location of the random word
+                // if we hit this, we must break out of for this loop and find another random word
+                break;
+            }
+            // continue the loop if:
+            // 1: the hash says that, at a particular location, we have a 1 - which is a "free space"
+            // 2: the hash has a character at a particular location, and our RNG word has a character at that location as well
+            randomWordPointer++;
+        }
+
+        // if we passed all of the tests, break out of the while loop
+        if (randomWordPointer === wordHeight) {
+            break;
+        } else {
+            randomWord = null;
+        }
+    }
+
+    // if execution is here, we have a valid vertical word that adheres to our constraints found in our hash
+    // We will now insert that word in the desired location
+
+    let insertPointer = 0;
+    for (let i = rowStart; i <= rowEnd; i++) {
+        grid[i][col] = randomWord[insertPointer];
+        insertPointer++;
+    }
+
+    return grid;
 }
